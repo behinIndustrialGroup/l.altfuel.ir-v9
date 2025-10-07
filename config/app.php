a1b432c98ac03a8dc2f718e4adca06fe189a0482
behinIndustrialGroup <info@behin.energy>
2025-10-07 09:07:23 +0330

    Revert "Add course registration package"
    This reverts commit ce6ebc26dc99bb7031afb894d136e2b82952bcf7.

--- config/app.php
+++ config/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use CourseRegistration\CourseRegistrationServiceProvider;
 use Registration\RegistrationServiceProvider;
 
 return [
@@ -253,7 +252,6 @@ return [
         \Behin\Complaint\ComplaintProvider::class,
         Behin\Hamayesh\BehinHamayeshServiceProvider::class,
         RegistrationServiceProvider::class,
-        CourseRegistrationServiceProvider::class,
         \TelegramTicket\TelegramTicketProvider::class,
 
         // \Mkhodroo\CaseInsensitiveTranslate\CaseInsensitiveTranslateProvider::class,
