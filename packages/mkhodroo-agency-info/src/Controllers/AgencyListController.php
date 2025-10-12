4e39e8c54fe3859397e79e27770b406a7ea26036
behinIndustrialGroup <166484385+behinIndustrialGroup@users.noreply.github.com>
2025-10-12 13:05:06 +0330

    Add advanced filtering to agency list

--- packages/mkhodroo-agency-info/src/Controllers/AgencyListController.php
+++ packages/mkhodroo-agency-info/src/Controllers/AgencyListController.php
@@ -49,7 +49,6 @@ class AgencyListController extends Controller
 
         $main_field_search = config('agency_info.main_field_name'). "_search";
         $main_field = config('agency_info.main_field_name');
-        $agencies = AgencyInfo::get();
         $parent_ids = [];
         if($r->$main_field_search){
             $parent_ids[] = AgencyInfo::where('key', $main_field)->where('value', $r->$main_field_search)->pluck('parent_id')->toArray();
@@ -67,6 +66,51 @@ class AgencyListController extends Controller
             $parent_ids[] = AgencyInfo::where('value' , 'like', "%". $r->field_value. "%")->pluck('parent_id')->toArray();
 
         }
+
+        $advanced_filters = collect($r->get('advanced_filters', []))
+            ->map(function ($filter) {
+                $filter['key'] = isset($filter['key']) ? trim($filter['key']) : '';
+                $filter['value'] = isset($filter['value']) ? trim($filter['value']) : '';
+                $filter['condition'] = isset($filter['condition']) ? trim($filter['condition']) : 'equals';
+                $filter['boolean'] = isset($filter['boolean']) ? strtolower(trim($filter['boolean'])) : 'and';
+                return $filter;
+            })
+            ->filter(function ($filter) {
+                return $filter['key'] !== '' && $filter['value'] !== '';
+            })
+            ->values();
+
+        if ($advanced_filters->isNotEmpty()) {
+            $advanced_parent_ids = [];
+            $advanced_filters->each(function ($filter, $index) use (&$advanced_parent_ids) {
+                $condition = $filter['condition'] === 'contains' ? 'contains' : 'equals';
+                $boolean = $filter['boolean'] === 'or' ? 'or' : 'and';
+
+                $query = AgencyInfo::where('key', $filter['key']);
+
+                if ($condition === 'contains') {
+                    $query->where('value', 'like', "%" . $filter['value'] . "%");
+                } else {
+                    $query->where('value', $filter['value']);
+                }
+
+                $current_parent_ids = $query->pluck('parent_id')->toArray();
+
+                if ($index === 0) {
+                    $advanced_parent_ids = $current_parent_ids;
+                    return;
+                }
+
+                if ($boolean === 'or') {
+                    $advanced_parent_ids = array_values(array_unique(array_merge($advanced_parent_ids, $current_parent_ids)));
+                } else {
+                    $advanced_parent_ids = array_values(array_intersect($advanced_parent_ids, $current_parent_ids));
+                }
+            });
+
+            $parent_ids[] = $advanced_parent_ids;
+        }
+
         $count = count($parent_ids);
         if($count > 1){
             $intersects = $parent_ids[0];
@@ -77,6 +121,13 @@ class AgencyListController extends Controller
         }
         if($count === 1){
             $parent_ids = $parent_ids[0];
+        }
+        if($count === 0){
+            return ['data' => []];
+        }
+        $parent_ids = array_values(array_unique($parent_ids));
+        if(empty($parent_ids)){
+            return ['data' => []];
         }
         $agencies = AgencyInfo::whereIn('id', $parent_ids)->groupBy('parent_id')->get();
 
