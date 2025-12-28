519f6ac97421d8ce9e5432509f97275416b07776
behinIndustrialGroup <info@behin.energy>
2025-12-28 16:47:56 +0330

    Update irngv.php

--- routes/irngv.php
+++ routes/irngv.php
@@ -12,7 +12,7 @@ Route::prefix('irngv')->group(function(){
 
     //شارژ
     Route::get('charge', [IrngvChargeController::class, 'index']);
-    Route::post('charge/pay', [IrngvChargeController::class, 'pay']);
-    Route::any('charge/verify', [IrngvChargeController::class, 'verify']);
-    Route::any('charge/status', [IrngvChargeController::class, 'status']);
+    Route::post('charge/pay', [IrngvChargeController::class, 'pay'])->name('irngv.charge.pay');
+    Route::any('charge/verify', [IrngvChargeController::class, 'verify'])->name('irngv.charge.verify');
+    Route::any('charge/status', [IrngvChargeController::class, 'status'])->name('irngv.charge.status');
 });
