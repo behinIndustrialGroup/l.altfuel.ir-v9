2e19737daa353600cf505bcf4e20a5aa204c2cf7
Amirali Hosseini <amiralihosseini216@yahoo.com>
2025-12-29 10:46:54 +0330

    debt debug 3.0

--- packages/mkhodroo-agency-info/src/Controllers/SendDebtToCrmController.php
+++ packages/mkhodroo-agency-info/src/Controllers/SendDebtToCrmController.php
@@ -329,7 +329,7 @@ class SendDebtToCrmController extends Controller
             $debtData = [
                 'rhs_name' => $debt['display_name'],
                 'rhs_amountowed' => floatval($debt['amount']),
-                'rhs_servicecentercode@odata.bind' => "/rhs_servicecenters($serviceCenterId)"
+                'rhs_ServiceCenterCode@odata.bind' => "/rhs_servicecenters($serviceCenterId)"
             ];
 
             // اضافه کردن تاریخ پرداخت اگر وجود داشت
